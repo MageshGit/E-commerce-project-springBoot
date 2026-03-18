b50f053c1c4ca4617b23069d81511f2a3ea69956
MageshGit <iammagesh15@gmail.com>
2026-03-18 22:33:13 +0530

    code cleanup

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/controller/AdminController.java
@@ -158,7 +158,6 @@ public class AdminController {
 	public String updateProduct(@PathVariable("id") int id ,@RequestParam("name") String name,@RequestParam("categoryid") int categoryId ,@RequestParam("price") int price,@RequestParam("weight") int weight, @RequestParam("quantity")int quantity,@RequestParam("description") String description,@RequestParam("productImage") String productImage)
 	{
 
-//		this.productService.updateProduct();
 		return "redirect:/admin/products";
 	}
 	
