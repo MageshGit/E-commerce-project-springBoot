b468681a3e5161c9f3b0fe247fc30a5684718154
Magesh <magesh.nagamani@aspiresys.com>
2026-04-07 12:11:01 +0530

    error fixes

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/userDao.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/dao/userDao.java
@@ -45,7 +45,7 @@ public class userDao {
     	try {
 			User user = (User) query.getSingleResult();
 			// System.out.println(user.getPassword());
-			if(password1.equals(user.getPassword())) {
+			if(password.equals(user.getPassword())) {
 				return user;
 			}else {		
 				return new User();
